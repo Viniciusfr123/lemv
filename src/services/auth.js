812d156f557d9235bc7af099674c47731ebaae99
ViniciusFR <viniciusrocha28@gmail.com>
2022-11-19 23:15:00 -0300

    provisional login

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,22 +1,41 @@
+// import { response } from "express"
 
 export default httpClient => ({
+  // login: async ({ matricula, senha }) => {
+  //   await httpClient.post('/auth', {
+  //     matricula,
+  //     senha
+  //   })
+  //   let errors = null
+
+  //   if (!response.data) {
+  //     errors = {
+  //       status: response.request.status,
+  //       statusText: response.request.statusText
+  //     }
+  //   }
+  //   return {
+  //     data: { token: response.data.token },
+  //     errors: null
+  //   }
+  // }
+
   login: async ({ matricula, senha }) => {
-    await httpClient.post('/auth', {
-      matricula,
-      senha
-    })
-    // let errors = null
-
-    // if (!response.data) {
-    //   errors = {
-    //     status: response.request.status,
-    //     statusText: response.request.statusText
-    //   }
-    // }
-
-    return {
-      data: { token: 'taliberado' },
-      errors: null
+    // await httpClient.post('/auth', {
+    //   matricula,
+    //   senha
+    // })
+    if (matricula === 'lemv.admin' && senha === 'aelv@42') {
+      return { data: { token: 'taliberado' }, errors: null }
+    } else {
+      console.log('caiu aqui')
+      return {
+        data: {},
+        errors: {
+          status: 404,
+          statusText: 'Login ou senha invalidos'
+        }
+      }
     }
   }
 })
