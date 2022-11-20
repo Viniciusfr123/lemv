34655093f5b1e13b5fc4e4bf218319f36f064e57
ViniciusFR <viniciusrocha28@gmail.com>
2022-11-19 23:56:40 -0300

    add text lemPrincipal

--- src/views/Inicio/LemPrincipal.vue
+++ src/views/Inicio/LemPrincipal.vue
@@ -5,13 +5,12 @@
         <div class="flex flex-col mt-20">
             <h1 class="flex text-4xl font-black justify-center text-black">Bem vindo ao LEM-V!</h1>
             <p class="text-lg font-medium mt-10 text-gray">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla<br>
-                 Nulla malesuada arcu at erat porttitor porttitor. Vestibulum<br>
-                 vel sodales diam sed vehicula. Aliquam malesuada quam placerat<br>
-                 ipsum non urna fermentum, id pharetra mauris porttitor.<br><br>
-                 Integer tincidunt nibh lectus, eget ullamcorper est ullamcorper sed.<br>
-                 convallis tincidunt pulvinar diam dictum dignissim. Suspendisse<br>
-                 sit amet ornare lectus consequat. Donec placerat arcu ut tincidunt blandit.
+                No dia 30 de agosto de 1989 foi criado o Laboratório de Ensino de <br>
+                Matemática (LEM) da Escola Técnica Federal do Espírito Santo (ETFES).<br>
+                O LEM contribui no desenvolvimento profissional dos professores<br>
+                ou futuros professores de matemática, do IFES ou de outras instituições.<br><br>
+                A consulta ao nosso acervo poderá ser realizada, com acesso a<br>
+                 alguns projetos e atividades, a partir deste ambiente.
             </p>
         </div>
     </div>
